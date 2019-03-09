4fd4c6562ce92f53645a74aeff64541290ca3479
Adalberto Junior <adalrsjr1@gmail.com>
2019-03-09 16:28:34 -0300

    done: crawler for clubefii

--- crawler-fii.py
+++ crawler-fii.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import argparse
 from os import path,stat
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -19,6 +20,10 @@ def load_fiis():
     else:
         html = request_html()
         save_html(html)
+    return html
+
+def fake_load_fiis():
+    html = load_html()
     return html
 
 def request_html():
@@ -46,24 +51,72 @@ def load_html():
     with open('html.cache', 'r') as f:
         return f.read()
 
-def print_node(node):
-    print(node)
-    for child in node:
-        print(child)
-        print_node(child)
-
 class Fii:
+
     def __init__(self, raw):
-        pass
+        tr = raw
+        children = tr.getchildren()
+
+        self.code = children[0].getchildren()[0].text
+        #self.name = children[1].getchildren()[0].text
+        try:
+            self.total = children[2].getchildren()[0].getchildren()[0].text
+            self.total = self._format_price(self.total)
+        except:
+            self.total = None
+        self.price = self._format_price(children[3].getchildren()[0].text)
+        self.yield1 = self._format_yield(children[4].getchildren()[0].text)
+        self.yield12 = self._format_yield(children[5].getchildren()[0].text)
+        self.type = children[6].getchildren()[0].text
+        self.ref = children[7].getchildren()[0].text
+        self.base = children[8].getchildren()[0].text
+        self.payment = children[9].getchildren()[0].text
+
+
+    def _format_price(self, text):
+        try:
+            text = text.replace('R$','').replace('.','').replace(',','.')
+            return float(text)
+        except:
+            return None
+
+    def _format_yield(self, text):
+        try:
+            text = text.replace('%','').replace(',','.')
+            return float(text)
+        except:
+            return None
 
     def __str__(self):
-        pass
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(\
+        self.code, \
+        self.total,\
+        self.price,\
+        self.yield1,\
+        self.yield12,\
+        self.ref,\
+        self.base,\
+        self.payment\
+        )
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bech', type=float)
+    parser.add_argument('--rf', type=float)
+    parser.add_argument('--risk',type=float)
+
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
+
     tableHTML = load_fiis()
     table_tree = html.fromstring(tableHTML)
     table_id = "table_data_fis"
     rows = table_tree.xpath("//tbody/tr")
 
     for row in rows:
-        print_node(row)
+        if 'tr' == row.tag:
+            fii = Fii(row)
+            print(fii)
