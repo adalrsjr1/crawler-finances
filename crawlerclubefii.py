299344a4b3fd1d626925238ad48d21d1a0d12342
Adalberto Junior <adalrsjr1@gmail.com>
2019-03-16 13:46:42 -0300

    finish: crawlerclubefii

--- crawlerclubefii.py
+++ crawlerclubefii.py
@@ -14,7 +14,7 @@ class CrawlerClubeFii:
         if path.isfile(filename):
             # time since last modification in minutes should be less than 1 hour
             if (time.time() - stat(filename).st_mtime) / 60 > 60:
-                html = self.request_html(url)
+                html = self.request_html(self.url)
                 self.save_html(html)
             else:
                 html = self.load_html()
@@ -59,87 +59,105 @@ class ParserClubeFiiTable:
     def parse_row(self, row):
         if 'tr' == row.tag:
             elements = row.xpath('.//td')
-            return [self._extract_info(elem) for elem in elements]
+            return [self._format_info(self._extract_info(elem)) for elem in elements[:7]]
         return []
 
     def _extract_info(self, elem):
-        text = elem.text
-        if None == text:
-            children = elem.xpath('.//a')
-            for child in children:
-                print(child.getchildren())
-            elements = [child.text for child in children]
-            text = elements[0]
-        return text
+        info = None
+        if 'td' != elem.tag:
+            return None
 
-    def _format_number(self, elem):
+        info = elem.xpath('.//a')[0]
+        children = info.getchildren()
+
+        if 0 == len(children):
+            return info.text
+        else:
+            return children[0].text
+
+    def _format_info(self, elem):
+        elem = elem.replace('\\r\\n','').strip()
+
+        if 'N/D' == elem:
+            return -1.0
+
         try:
-            n = float(elem.replace('R$','').replace('.','').replace(',','.'))
-            return n
+            return float(elem.replace('R$','').replace('%','').replace('.','').replace(',','.'))
         except:
             return elem
 
 class Fii:
-    def __init__(self, values):
+    def __init__(self, discount_rate, rf, risk_rate, values):
+        self.discount_rate = discount_rate
+        self.rf = rf
+        self.risk_rate = risk_rate
         self.code = values[0]
-        self.price = values[1]
-        self.shareholdersequity = values[2]
-        self.equity = values[3]
-        self.yield1 = values[4]
-        self.yield12 = values[5]
-        self.volsells = values[6]
-        self.volequity = values[7]
-        self.obs = values[8]
+        self.name = values[1]
+        self.price = values[2]
+        self.equity_price = values[3]
+        self.yield1 = values[4]/100
+        self.yield12 = values[5]/100
+        self.type = values[6][0]
 
     def __str__(self):
-        return "{}\t{}\t{}".format(\
-        self.code,\
+        color = '\033[92m' # green
+        endcolor = '\033[0m'
+        if self.downside():
+            color = '\33[91m'
+
+        return '{}{:>10} R$ {:<10.2f} R$ {:<10.2f} R$ {:<10.2f} {:5.2f}% {:10.4f} {} {}'.format(\
+        color,\
+        self.code, \
         self.price,\
-        self.equity\
+        self.fair_price(),
+        self.equity_price,\
+        self.average_yield()*100,\
+        self.downside_value(),\
+        'D' if self.downside() else 'U',\
+        endcolor,\
         )
 
-    def  equityperc(self):
-        return self.equity / self.price
+    def  equity_price(self):
+        return self.equity * self.price
 
-    def yield1perc(self):
-        return self.yield1 / self.price
+    def yield1_price(self):
+        return self.yield1 * self.price
 
-    def yield12perc(self):
-        return ((1.0 + self.yield1perc()) ** 12) - 1
+    def yield12_price(self):
+        return self.yield12 * self.price
 
-    def finaldiscountrate(self, rate, rf, risk_rate):
-        return rate * (1-rf) * (1+risk_rate)
+    def final_discount_rate(self):
+        return self.discount_rate * (1-self.rf) * (1+self.risk_rate)
 
-    def monthlydiscounterate(self, final_discount_rate):
+    def monthly_discounte_rate(self):
         # http://fazaconta.com/taxa-mensal-vs-anual.htm
-        return ((1+final_discount_rate) ** 1/12) - 1
+        return ((1+self.final_discount_rate()) ** (1/12))-1
 
-    def fairprice(self, final_discount_rate):
-        price = self.yield12 / self.monthlydiscounterate(final_discount_rate)
-        return (price, price/self.price)
+    def downside(self):
+        return self.yield1 < self.monthly_discounte_rate()
 
-    def __str__(self):
-        final_discount_rate = self.finaldiscountrate(0.1248, 0.175, 0.10)
-        return "{}\tR$ {}\t{}\t{}\t{}\t{} {}".format(\
-            self.code, \
-            self.price, \
-            self.yield1, \
-            self.monthlydiscounterate(final_discount_rate), \
-            self.yield12, \
-            final_discount_rate, \
-            self.fairprice(final_discount_rate)
-        )
+    def downside_value(self):
+        return self.fair_price_perc() - 1
+
+    def average_yield(self):
+        return ((self.yield12 + 1) ** (1/12)) - 1
+
+    def fair_price(self):
+        price = (self.yield1_price() / self.monthly_discounte_rate())
+        return price 
+
+    def fair_price_perc(self):
+        return self.fair_price()/self.price
 
 if __name__ == '__main__':
+    #print(Fii(12.48/100, 0.1750, 0.10, ['XXXX', '', 100.00, 0, 0.83, 0.1047, 'a']))
+
     crawler = CrawlerClubeFii()
     content = crawler.load_fiis()
 
     parser = ParserClubeFiiTable(content)
+
     for row in parser.parse_table():
         parsed_row = parser.parse_row(row)
-        print(parsed_row)
-
-    #for row in table:
-    #    print(row)
-        #fii = Fii(crawler.parserow(row))
-        #print("{}".format(fii))
+        fii = Fii(6.4492/100, 0.175, 0.20, parsed_row)
+        print(fii)
