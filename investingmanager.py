a6e04702c48a6b77a93471de8b2f11162cd011bc
Adalberto Junior <adalrsjr1@gmail.com>
2019-08-30 11:49:05 -0300

    add more refs

--- investingmanager.py
+++ investingmanager.py
@@ -2,3 +2,4 @@
 # http://wilsonfreitas.github.io/slides/ser-maio-2018/index.html#1 
 # stocks API
 # https://www.alphavantage.co/documentation/
+# https://github.com/jonatascp/api-investments/tree/master/helpers
