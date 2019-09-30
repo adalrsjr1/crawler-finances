d273c706b654a1816850a99aaf2efe1719091dad
Adalberto Junior <adalrsjr1@gmail.com>
2019-09-30 16:21:28 -0300

    fix content-user bug

--- crawlerclubefii.py
+++ crawlerclubefii.py
@@ -9,7 +9,7 @@ class CrawlerClubeFii:
         self.url = 'https://www.clubefii.com.br/proventos-rendimento-distribuicoes-amortizacoes_ajx?verifica_ultimo_provento=false'
 
     def load_fiis(self):
-        filename = 'html.cache'
+        filename = 'cache.html'
         html = ''
         if path.isfile(filename):
             # time since last modification in minutes should be less than 1 hour
@@ -28,15 +28,16 @@ class CrawlerClubeFii:
         return html
 
     def save_html(self, content):
-        with open('html.cache', 'w') as f:
+        with open('cache.html', 'w') as f:
             f.write(str(content))
 
     def load_html(self):
-        with open('html.cache', 'r') as f:
+        with open('cache.html', 'r') as f:
             return f.read()
 
     def request_html(self, url):
-        r = requests.get(self.url)
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'}
+        r = requests.get(url, headers=headers)
         html_page = r.content
         return html_page
 
